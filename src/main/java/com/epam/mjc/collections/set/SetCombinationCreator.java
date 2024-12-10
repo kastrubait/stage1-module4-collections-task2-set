16aa5c7ba0bb74793f89416290a486ac29c29332
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-12-10 12:29:25 +0300

    feat: complete SetCombinationCreator module4 stage1 v.0

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,18 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+
+        Set<String> intersectionSet1And2 = new HashSet<>(firstSet);
+        intersectionSet1And2.retainAll(secondSet);
+        intersectionSet1And2.removeAll(thirdSet);
+
+        Set<String> set3Only = new HashSet<>(thirdSet);
+        set3Only.removeAll(firstSet);
+        set3Only.removeAll(secondSet);
+
+        Set<String> resultSet = new HashSet<>(intersectionSet1And2);
+        resultSet.addAll(set3Only);
+
+        return resultSet;
     }
 }
