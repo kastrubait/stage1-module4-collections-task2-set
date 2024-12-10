59ec7448db10beb516f8e58bcf5c137a285beee1
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-12-10 11:48:36 +0300

    feat: complete HashSetCreator module4 stage1 v.0

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,23 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (Integer num : sourceList) {
+            if (num % 2 != 0) {
+                set.add(num);
+                set.add(2 * num);
+            } else {
+                set.add(num);
+                int element = num;
+                while((element % 2) == 0) {
+                    element = element / 2;
+                    set.add(element);
+                }
+            }
+        }
+
+        return set;
     }
 }
