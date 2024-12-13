91f4b72810a291245c27cd4c43d1c114a0430d7f
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-12-13 10:22:57 +0300

    feat: complete SubsetofSquaresCreator module4 stage1 v.0

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,18 @@
 package com.epam.mjc.collections.set;
 
+import java.util.TreeSet;
 import java.util.List;
 import java.util.Set;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+
+        TreeSet<Integer> squareSet = new TreeSet<>();
+
+        for (Integer num : sourceList) {
+            squareSet.add(num * num);
+        }
+
+        return squareSet.subSet(lowerBound, true, upperBound, true);
     }
 }
